95e3d649786619aa32dc550c953ad5cb7073c414
Andrey Bakulin <hansss@list.ru>
2024-01-23 17:08:20 +0400

    step 1.0

--- src/index.js
+++ src/index.js
@@ -9,30 +9,25 @@ const pageLoader = (url, outputPath) => {
     .replace(/[^a-zA-Z0-9_-]/g, '-')
     .replace(/[^\w\d]+$/, '')}.html`;
   const filePath = path.resolve(outputPath, filename);
-
-	const agent = new https.Agent({
-		rejectUnauthorized: false,
-		minVersion: 'TLSv1.2',
-	});
-
-  return axios
-    .get(url, { httpsAgent: agent })
-    .then((resp) => resp)
-    .then((pageContent) => fs.writeFile(filePath, pageContent, 'utf-8'))
-    .then(() => `Page was succcesfully downloaded into '${filePath}'`)
-    .catch((error) => {
-      console.log('ERROR!!!', error);
-      switch (error.name) {
-        case 'Error':
-          console.log('Error:', error.message);
-          break;
-        case 'AxiosError':
-          console.log('Error:', error.message);
-          break;
-        default:
-          console.log(error);
-      }
-    });
+	
+	return axios
+		.get(url)
+		.then((resp) => resp)
+		.then((pageContent) => fs.writeFile(filePath, pageContent, 'utf-8'))
+		.then(() => `Page was successfully downloaded into '${filePath}'`)
+		.catch((error) => {
+			console.log('ERROR!!!', error);
+			switch (error.name) {
+				case 'Error':
+					console.log('Error:', error.message);
+					break;
+				case 'AxiosError':
+					console.log('Error:', error.message);
+					break;
+				default:
+					console.log(error);
+			}
+		});
 };
 
 export default pageLoader;
